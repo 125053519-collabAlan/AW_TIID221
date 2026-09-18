e4a8e97ca2a201c761a562d58e4f7ca8519f2a78
125053519-collabAlan <125053519@upq.edu.mx>
2026-09-18 12:10:22 -0600

    Realizamos un archivo php y utilizamos laragon para poder visualizar en la web

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     <h1 style="color: #0000FF;">Titulo</h1>
+     <h2>Subtitulo</h2>
+     <h3>Subtitulo 2</h3>
+     <h4>Subtitulo 3</h4>
+     <h5>Subtitulo 4</h5>
+     <h6>Subtitulo 5</h6>
+     
+</body>
+</html>
