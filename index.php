f5205335be54907b086c74e5127bae3fe4e05895
125053519-collabAlan <125053519@upq.edu.mx>
2026-09-18 19:23:04 -0600

    Finalizacion de la practica de ayer y hoy con uso de bordes y margenes

--- index.php
+++ index.php
@@ -4,14 +4,58 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
+     
+     <style>
+     .seccion1 {
+     background-color: lightgreen;
+     padding: 20px 40px;
+     margin: 20px;
+     border: 10px hidden black;
+     }
+
+     .seccion2 {
+     background-color: lightpink;
+     padding: 20px 40px;
+     margin-top: 20px;
+     margin-right: 30px;
+     margin-bottom: 40px;
+     margin-left: 50px;
+     }
+
+</style>
+
 </head>
 <body>
-     <h1 style="color: #0000FF;">Titulo</h1>
-     <h2>Subtitulo</h2>
-     <h3>Subtitulo 2</h3>
-     <h4>Subtitulo 3</h4>
-     <h5>Subtitulo 4</h5>
-     <h6>Subtitulo 5</h6>
+     <h1 style="color: #0000FF;">Hola crayola</h1>
+     <h2>Hola crayola</h2>
+     <h3>Hola crayola</h3>
+     <h4>Hola crayola</h4>
+     <h5>Hola crayola</h5>
+     <h6>Hola crayola</h6>
+
+<div class="seccion1">
+     <h2>Título del párrafo</h2>
+
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum</p>
+</div>
+
+<div class="seccion2">
+     <h2>Título del párrafo</h2>
+
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum</p>
+</div>
      
 </body>
 </html>
